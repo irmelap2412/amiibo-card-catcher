921fa3355125fec189757d261cb103d8113b5e1c
irmelap2412 <irmela.prcic@fet.ba>
2026-05-30 17:48:43 +0200

    change vite to accept selected hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,11 @@ export default defineConfig({
   plugins: [react()],
     server: {
     host: '0.0.0.0',
-    allowedHosts: 'all',
+    allowedHosts: [
+      'amiibo-alb-273469495.us-east-1.elb.amazonaws.com', // Tvoj konkretni Load Balancer
+      '.elb.amazonaws.com',                              // Wildcard koji dopušta AWS provjere
+      'localhost'                                        // Za lokalno testiranje ako zatreba
+    ],
     port: 3000,
     watch: {
       usePolling: true
